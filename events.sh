f4250178a1dcb07cd88f427806f02b902a7157b9
CoolElectronics <me@coolelectronics.me>
2024-06-30 19:55:49 -0400

    update autorespond message and index page

--- events.sh
+++ events.sh
@@ -15,7 +15,5 @@ if [[ $1 == "tag" ]]; then
   actorlookup "$tagger"
     
 
-  ./ctl.sh act post "$uid" "@$setAcct hi!" "$noteid"
-
-  echo ./ctl.sh act post "$uid" "$setAcct hi!" "$noteid" "$setUrl"
+  ./ctl.sh act post "$uid" "@$setAcct :3" "$noteid"
 fi
